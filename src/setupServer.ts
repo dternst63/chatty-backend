37ab02f113a230c6666f2d77b42492ef60917813
dternst63 <dternst63@gmail.com>
2024-07-16 13:21:48 -0500

    save current lesson 071624

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -11,9 +11,10 @@ import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
 import Logger from 'bunyan';
 import 'express-async-errors';
-import { config } from './config';
-import applicationRoutes from './routes';
-import { CustomError, IErrorResponse } from './shared/global/helpers/error-handler';
+import { config } from '@root/config';
+import applicationRoutes from '@root/routes';
+import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
+
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
@@ -110,5 +111,7 @@ export class ChattyServer {
     });
   }
 
-  private socketIOConnections(io: Server): void {}
+  private socketIOConnections(io: Server): void {
+    log.info('socketIOConnections');
+  }
 }

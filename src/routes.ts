f2a406032ffa2f219fe62e20e5d2e5dfe6819fa8
dternst63 <dternst63@gmail.com>
2024-07-22 14:26:48 -0500

    worked accomplished on 07/22/20244

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,14 @@
+import { authRoutes } from '@auth/routes/auth.route';
+import { serverAdapter } from '@service/queues/base.queue';
 import { Application } from 'express';
 
+const BASE_PATH = '/api/v1';
+
 export default (app: Application) => {
-  const routes = () => {};
+  const routes = () => {
+    app.use('/queues', serverAdapter.getRouter());
+    app.use(BASE_PATH, authRoutes.routes());
+    app.use(BASE_PATH, authRoutes.signOutRoute());
+  };
   routes();
 };
